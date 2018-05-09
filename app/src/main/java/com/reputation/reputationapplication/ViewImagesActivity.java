364b83873f8566aed854db8afe8c52f0ee163df6
Purushotham-PC\Purushotham <kpreddy554@gmail.com>
2018-05-09 12:35:31 +0530

    refactiring 2

--- app/src/main/java/com/reputation/reputationapplication/ViewImagesActivity.java
+++ app/src/main/java/com/reputation/reputationapplication/ViewImagesActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package com.reputation.reputationapplication;
 
 import android.app.ProgressDialog;
@@ -51,7 +36,7 @@ public class ViewImagesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download_selection);
+        setContentView(R.layout.activity_view_images);
         util = new Util();
         initData();
         initUI();
@@ -101,7 +86,7 @@ public class ViewImagesActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             dialog = ProgressDialog.show(ViewImagesActivity.this,
-                    getString(R.string.refreshing),
+                    getString(R.string.loading),
                     getString(R.string.please_wait));
         }
 
